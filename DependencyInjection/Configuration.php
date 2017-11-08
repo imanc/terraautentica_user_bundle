c671be0f99f5f33a50dd09e6e0f2f0914fdb84c2
dariomancini <imanc86@gmail.com>
2017-11-08 12:09:56 +0100

    first commit

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,106 @@
+<?php
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\UserBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files.
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('sonata_user');
+
+        $supportedManagerTypes = array('orm', 'mongodb');
+
+        $rootNode
+            ->children()
+                ->booleanNode('security_acl')->defaultFalse()->end()
+                ->arrayNode('table')
+                    ->addDefaultsIfNotSet()
+                    ->children()
+                        ->scalarNode('user_group')->defaultValue('fos_user_user_group')->end()
+                    ->end()
+                ->end()
+                ->scalarNode('impersonating_route')->end()
+                ->arrayNode('impersonating')
+                    ->children()
+                        ->scalarNode('route')->defaultFalse()->end()
+                        ->arrayNode('parameters')
+                            ->useAttributeAsKey('id')
+                            ->prototype('scalar')->end()
+                        ->end()
+                    ->end()
+                ->end()
+                ->arrayNode('google_authenticator')
+                    ->addDefaultsIfNotSet()
+                    ->children()
+                        ->scalarNode('server')->cannotBeEmpty()->end()
+                        ->scalarNode('enabled')->defaultFalse()->end()
+                    ->end()
+                ->end()
+                ->scalarNode('manager_type')
+                    ->defaultValue('orm')
+                    ->validate()
+                        ->ifNotInArray($supportedManagerTypes)
+                        ->thenInvalid('The manager type %s is not supported. Please choose one of '.json_encode($supportedManagerTypes))
+                    ->end()
+                ->end()
+                ->arrayNode('class')
+                    ->children()
+                        ->scalarNode('group')->cannotBeEmpty()->end()
+                        ->scalarNode('user')->cannotBeEmpty()->end()
+                    ->end()
+                ->end()
+                ->arrayNode('admin')
+                    ->addDefaultsIfNotSet()
+                    ->children()
+                        ->arrayNode('group')
+                            ->addDefaultsIfNotSet()
+                            ->children()
+                                ->scalarNode('class')->cannotBeEmpty()->end()
+                                ->scalarNode('controller')->cannotBeEmpty()->defaultValue('SonataAdminBundle:CRUD')->end()
+                                ->scalarNode('translation')->cannotBeEmpty()->defaultValue('SonataUserBundle')->end()
+                            ->end()
+                        ->end()
+                        ->arrayNode('user')
+                            ->addDefaultsIfNotSet()
+                            ->children()
+                                ->scalarNode('class')->cannotBeEmpty()->end()
+                                ->scalarNode('controller')->cannotBeEmpty()->defaultValue('SonataAdminBundle:CRUD')->end()
+                                ->scalarNode('translation')->cannotBeEmpty()->defaultValue('SonataUserBundle')->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+
+                ->arrayNode('profile')
+                    ->addDefaultsIfNotSet()
+                    ->children()
+                        ->scalarNode('default_avatar')->defaultValue('bundles/sonatauser/default_avatar.png')->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
---
+++ Resources/doc/index.rst
@@ -0,0 +1,15 @@
+User Bundle
+===========
+
+.. toctree::
+   :caption: Reference Guide
+   :name: reference-guide
+   :maxdepth: 1
+   :numbered:
+
+   reference/introduction
+   reference/installation
+   reference/advanced_configuration
+   reference/two_step_validation
+   reference/api
+   reference/user_impersonation
